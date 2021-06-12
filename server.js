31da8c7f6f470eefc09395af4fe6912a76637b61
Subhash Shrestha <subhashshrestha3@gmail.com>
2021-06-12 20:54:16 +0930

    server.js created with functionalities

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const PORT = process.env.PORT || 3000;
+app.use(express.static('public'));
+
+app.get("/",(req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+});
+
+
+http.listen(PORT,()=>{
+    console.log(`listening on ${PORT}`);
+});
+
+//socket
+let users = {};
+const io = require("socket.io")(http);
+
+io.on("connection", socket=>{
+    socket.on("new-member-joined", name=>{
+        users[socket.id] = name;
+        socket.broadcast.emit("new-member-joined", name);
+    });
+
+    socket.on("message",(msg)=>{
+        socket.broadcast.emit("message", {message:msg, name:users[socket.id]});
+    });
+
+    socket.on("disconnect", msg=>{
+        socket.broadcast.emit("left-chat",{name:users[socket.id]});
+        delete users[socket.id];
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// const io = require("socket.io")(8000);
+
+// const users = {};
+
+// io.on("connection", socket => {
+//     socket.on("new-user-joined", name => {
+//         console.log("Your name::: ",name);
+//         users[socket.id] = name;
+//         socket.broadcast.emit("user-joined", name);
+//     });
+
+//     socket.on("send", message => {
+//         socket.broadcast.emit("receive", { message: message, name: users[socket.id] });
+//     });
+// });
+
